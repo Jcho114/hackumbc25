42a5ffe46a67c114af20dec7d6eaf8f0ce01ba3a
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2025-09-27 15:37:30 -0400

    feat: session init metadata check and import/export

--- server/main.py
+++ server/main.py
@@ -1,6 +1,110 @@
-def main():
-    print("Hello from server!")
+from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi.responses import FileResponse, JSONResponse
+from uuid import uuid4
+
+import os
+import json
+import pandas as pd
+import shutil
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.post("/session/init")
+def init(session_name: str):
+    # create session directory
+    session_id = str(uuid4())
+    os.mkdir(session_id)
+
+    # create metadata file (include name in file)
+    with open(os.path.join(session_id, "metadata.json"), "w") as f:
+        json.dump({"session_name": session_name}, f)
+
+    # return session id
+    return session_id
+
+
+@app.get("/session/{session_id}/metadata")
+def get_metadata(session_id: str):
+    file_path = f"{session_id}/metadata.json"
+
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="Metadata file not found")
+
+    with open(os.path.join(session_id, "metadata.json"), "r") as file:
+        try:
+            metadata = json.load(file)
+        except json.JSONDecodeError:
+            raise HTTPException(status_code=500, detail="Error decoding JSON from file")
+
+    return JSONResponse(content=metadata)
+
+
+@app.post("/session/{session_id}/upload")
+def upload(session_id: str, file: UploadFile):
+    # todo - do csv check
+    if file.content_type != "text/csv":
+        raise HTTPException(status_code=400, detail="File is not csv")
+    # todo - check session id is valid
+
+    # make node id
+    node_id = str(uuid4())
+
+    try:
+        with open(os.path.join(session_id, f"{node_id}.csv"), "wb") as f:
+            shutil.copyfileobj(file.file, f)
+
+        with open(os.path.join(session_id, "metadata.json")) as f:
+            metadata = json.loads(f.read())
+            if "nodes" not in metadata:
+                metadata["nodes"] = []
+            metadata["nodes"].append(node_id)
+
+        with open(os.path.join(session_id, "metadata.json"), "w") as f:
+            json.dump(metadata, f)
+
+        return {"node_id": node_id}
+    except Exception:
+        raise HTTPException(status_code=500, detail="Failed to upload file")
+
+
+@app.post("/session/{session_id}/export/{node_id}")
+def export(session_id: str, node_id: str):
+    file_path = os.path.join(session_id, f"{node_id}.csv")
+
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File not found")
+
+    return FileResponse(file_path)
+
+
+@app.post("/tools/sum")
+def tools_sum(dataset):
+    # Sum (Dataset, column, [groupbycolumn]) -> Scalar
+
+    pass
+
+
+@app.post("/tools/filter")
+def tools_filter():
+    pass
+
+
+@app.post("/tools/mean")
+def tools_mean():
+    pass
+
+
+@app.post("/tools/min")
+def tools_min():
+    pass
+
+
+@app.post("/tools/max")
+def tools_max():
+    pass
+
+
+@app.post("/tools/describe")
+def tools_describe():
+    pass
